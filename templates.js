7465124f25a30cd1fe4a7578265936e11c72279c
Rhett Applestone <rhettapples(at symbol)gmail.com>
2025-06-28 02:46:07 -0500

    Pushed with qcom

---
+++ templates.js
@@ -0,0 +1,14 @@
+document.getElementById("navbar-component").innerHTML = `
+	    	<div class="navbar">
+    		<ul>
+				<li><a href="./index.html">Home</a></li>
+				<li><a href="https://docs.google.com/forms/d/e/1FAIpQLSd0NfSkY5_V--a5OK0q122AdZTKsg6Pes_RVIbunewM3v8M1g/viewform">Application</a></li>
+				<li><a href="https://discord.gg/DRdhJpzJbH">Discord</a></li>
+				<li><a href="https://www.instagram.com/druidcollectiveua/">Instagram</a></li>
+				<li><a href="https://calendar.google.com/calendar/u/0?cid=MWUzYTNjZmZkNGIzMzZiOGIwMWUzZTQ4YTM0MzExNmU3MzI3MGUxNTllMjA4NzIwYmY5NDNjN2I1ZGUwMGY2OEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t">Calendar</a></li>
+				<li><a href="https://mysource.ua.edu/organization/malletassemblyhonorsresidenceprogram">UA Source</a></li>
+				<li><a href="mailto:rapplestone@crimson.ua.edu">Contact</a></li>
+			</ul>
+    	</div>
+
+`;
